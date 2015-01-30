076d318818d005620790cf55fdd0606e40201bd8
Ryan Slyter <rabemensch@gmail.com>
2015-01-30 15:59:40 -0800

    fixed issue with convert hex strings in files to unsigned shorts

--- block_cipher.c
+++ block_cipher.c
@@ -2,48 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 int main(void){
   
    FILE *fd = NULL; 
    size_t result;
    char block[16];
-   unsigned int w1, w2, w3, w4;
+   uint16_t w1, w2, w3, w4;
   
    if ((fd = fopen("plaintext.txt", "r")) == NULL){
       printf("Plaintext file open failed. Exit\n");
       exit(1); 
    }
-   char temp[4];
+   char temp1[4];
+   unsigned int temp2;
    while ((result = fread(block, 1, 16, fd)) == 16){             
-      //print_hex(block); DEBUG     
-      temp[0] = block[0];
-      temp[1] = block[1];
-      temp[2] = block[2];
-      temp[3] = block[3];
-      sscanf(temp, "%X", &w1);      
-      printf("%X\n", (unsigned int) w1);
+          
+      temp1[0] = block[0];
+      temp1[1] = block[1];
+      temp1[2] = block[2];
+      temp1[3] = block[3];
+      sscanf(temp1, "%X", &temp2);
+      w1 = (uint16_t) temp2;      
+      printf("%hx\n", w1);
+      printf("%hu\n", (uint16_t) w1);
+    
 
-      temp[0] = block[4];
-      temp[1] = block[5];
-      temp[2] = block[6];
-      temp[3] = block[7];
-      sscanf(temp, "%X", &w2);      
-      printf("%X\n", (unsigned int) w2);
+      temp1[0] = block[4];
+      temp1[1] = block[5];
+      temp1[2] = block[6];
+      temp1[3] = block[7];
+      sscanf(temp1, "%X", &temp2);
+      w2 = (uint16_t) temp2;
+      printf("%hx\n", w2);      
+      printf("%hu\n", (uint16_t) w2);
 
-      temp[0] = block[8];
-      temp[1] = block[9];
-      temp[2] = block[10];
-      temp[3] = block[11];
-      sscanf(temp, "%X", &w3);
-      printf("%X\n", (unsigned int) w3);
+      temp1[0] = block[8];
+      temp1[1] = block[9];
+      temp1[2] = block[10];
+      temp1[3] = block[11];
+      sscanf(temp1, "%X", &temp2);
+      w3 = (uint16_t) temp2;
+      printf("%hx\n", w3);
+      printf("%hu\n", (uint16_t) w3);
 
-      temp[0] = block[12];
-      temp[1] = block[13];
-      temp[2] = block[14];
-      temp[3] = block[15];
-      sscanf(temp, "%X", &w4);
-      printf("%X\n", (unsigned int) w4); 
+      temp1[0] = block[12];
+      temp1[1] = block[13];
+      temp1[2] = block[14];
+      temp1[3] = block[15];
+      sscanf(temp1, "%X", &temp2);
+      w4 = (uint16_t) temp2;
+      printf("%hx\n", w4);
+      printf("%hu\n", (uint16_t) w4); 
                   
    }
     
